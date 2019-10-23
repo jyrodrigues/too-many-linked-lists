153b064a0bb2d38feaa8c6357b245eebacabf194
José Rodrigues <jyrodrigues@gmail.com>
2019-10-23 11:24:29 -0300

    Finish 2.5

--- src/first.rs
+++ src/first.rs
@@ -19,13 +19,23 @@ impl List {
     List { head: Link::Empty }
   }
 
-  pub fn push(&mut self, val: i32) {
+  pub fn push(&mut self, elem: i32) {
     let new_node = Box::new(Node {
-      elem: val,
+      elem: elem,
       next: mem::replace(&mut self.head, Link::Empty),
     });
 
     self.head = Link::More(new_node);
+  }
+
+  pub fn pop(&mut self) -> Option<i32> {
+    match mem::replace(&mut self.head, Link::Empty) {
+      Link::Empty => None,
+      Link::More(node) => {
+        self.head = node.next;
+        Some(node.elem)
+      }
+    }
   }
 }
 

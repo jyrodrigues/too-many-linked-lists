a10ee84a2eaff046100e2aace80c227e78d6cad0
José Rodrigues <jyrodrigues@gmail.com>
2019-10-23 14:46:28 -0300

    Finish 3.4

--- src/second.rs
+++ src/second.rs
@@ -9,6 +9,8 @@ struct Node<T> {
   next: Link<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
 impl<T> List<T> {
   pub fn new() -> Self {
     List { head: None }
@@ -41,6 +43,10 @@ impl<T> List<T> {
       &mut node.elem
     })
   }
+
+  pub fn into_iter(self) -> IntoIter<T> {
+    IntoIter(self)
+  }
 }
 
 impl<T> Drop for List<T> {
@@ -50,6 +56,24 @@ impl<T> Drop for List<T> {
     while let Some(mut boxed_node) = cur_link {
       cur_link = boxed_node.next.take();
     }
+  }
+}
+
+// This works but isn't ideal: at any point in time we could call list.next()
+// after a list.push(). So basically it'd be a different kind of list.pop().
+//impl<T> Iterator for List<T> {
+//  type Item = T;
+//  fn next(&mut self) -> Option<Self::Item> {
+//    self.pop()
+//  }
+//}
+//
+// So here is a proper impl:
+
+impl<T> Iterator for IntoIter<T> {
+  type Item = T;
+  fn next(&mut self) -> Option<Self::Item> {
+    self.0.pop()
   }
 }
 
@@ -100,6 +124,22 @@ mod test {
 
     assert_eq!(list.peek(), Some(&432348));
     assert_eq!(list.pop(), Some(432348));
+  }
+
+  #[test]
+  fn into_iter() {
+    let mut list = List::new();
+
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter = list.into_iter();
+
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None);
   }
 }
 

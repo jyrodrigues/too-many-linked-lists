2fa6b2f0be5fc68fdfacbb58443a47546918ca90
José Rodrigues <jyrodrigues@gmail.com>
2019-10-28 22:00:42 -0300

    Road to final 4/?? - Implemented push_back, pop_back

--- src/unsafe_deque.rs
+++ src/unsafe_deque.rs
@@ -49,13 +49,55 @@ impl<T> List<T> {
         None => {
           self.tail = ptr::null_mut();
         }
-        new_head => {
-          self.head = new_head;
+        Some(mut new_head) => {
+          new_head.prev = ptr::null_mut();
+          self.head = Some(new_head);
         }
       }
 
       old_head.elem
     })
+  }
+
+  pub fn push_back(&mut self, elem: T) {
+    let mut new_tail = Box::new(Node {
+      elem: elem,
+      next: None,
+      prev: self.tail,
+    });
+
+    self.tail = &mut *new_tail;
+
+    if new_tail.prev.is_null() {
+      self.head = Some(new_tail);
+    } else {
+      unsafe {
+        let prev: &mut Node<T> = &mut *new_tail.prev;
+        prev.next = Some(new_tail);
+      }
+    }
+  }
+
+  pub fn pop_back(&mut self) -> Option<T> {
+    if self.tail.is_null() {
+      return None;
+    }
+
+    #[allow(unused_assignments)]
+    let mut pop_node: Option<Box<Node<T>>> = None;
+
+    unsafe {
+      if (*self.tail).prev.is_null() {
+        pop_node = self.head.take();
+        self.tail = ptr::null_mut();
+      } else {
+        let prev = &mut (*(*self.tail).prev);
+        pop_node = prev.next.take();
+        self.tail = &mut *prev;
+      }
+    }
+
+    pop_node.map(|node| node.elem)
   }
 
   pub fn peek(&self) -> Option<&T> {
@@ -138,7 +180,7 @@ mod test {
   use super::List;
 
   #[test]
-  fn basics() {
+  fn basics_front() {
     let mut list = List::new();
 
     assert!(list.pop_front().is_none());
@@ -158,6 +200,116 @@ mod test {
 
     assert_eq!(list.pop_front(), Some(1));
     assert_eq!(list.pop_front(), None);
+    assert_eq!(list.pop_front(), None);
+  }
+
+  #[test]
+  fn basics_back() {
+    let mut list = List::new();
+
+    assert!(list.pop_back().is_none());
+
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+
+    assert_eq!(list.pop_back(), Some(3));
+    assert_eq!(list.pop_back(), Some(2));
+
+    list.push_back(4);
+    list.push_back(5);
+
+    assert_eq!(list.pop_back(), Some(5));
+    assert_eq!(list.pop_back(), Some(4));
+
+    assert_eq!(list.pop_back(), Some(1));
+    assert_eq!(list.pop_back(), None);
+    assert_eq!(list.pop_back(), None);
+  }
+
+  #[test]
+  fn basics_queue_front() {
+    let mut list = List::new();
+
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+
+    assert_eq!(list.pop_back(), Some(1));
+    assert_eq!(list.pop_back(), Some(2));
+    assert_eq!(list.pop_back(), Some(3));
+    assert_eq!(list.pop_back(), None);
+
+    list.push_front(4);
+    list.push_front(5);
+
+    assert_eq!(list.pop_back(), Some(4));
+    assert_eq!(list.pop_back(), Some(5));
+    assert_eq!(list.pop_back(), None);
+  }
+
+  #[test]
+  fn basics_queue_back() {
+    let mut list = List::new();
+
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+
+    assert_eq!(list.pop_front(), Some(1));
+    assert_eq!(list.pop_front(), Some(2));
+    assert_eq!(list.pop_front(), Some(3));
+    assert_eq!(list.pop_front(), None);
+
+    list.push_back(4);
+    list.push_back(5);
+
+    assert_eq!(list.pop_front(), Some(4));
+    assert_eq!(list.pop_front(), Some(5));
+    assert_eq!(list.pop_front(), None);
+  }
+
+  #[test]
+  fn basics_queue_front_and_back() {
+    let mut list = List::new();
+
+    list.push_front(-2);
+
+    assert_eq!(list.pop_back(), Some(-2));
+    assert_eq!(list.pop_front(), None);
+    assert_eq!(list.pop_back(), None);
+
+    list.push_back(-1);
+
+    assert_eq!(list.pop_front(), Some(-1));
+    assert_eq!(list.pop_back(), None);
+    assert_eq!(list.pop_front(), None);
+
+    list.push_back(1);
+    list.push_front(2);
+    list.push_back(3);
+    list.push_front(4);
+
+    assert_eq!(list.pop_front(), Some(4));
+    assert_eq!(list.pop_back(), Some(3));
+    assert_eq!(list.pop_front(), Some(2));
+    assert_eq!(list.pop_back(), Some(1));
+
+    assert_eq!(list.pop_front(), None);
+    assert_eq!(list.pop_back(), None);
+
+    list.push_back(5);
+    list.push_front(6);
+    list.push_back(7);
+    list.push_front(8);
+
+    assert_eq!(list.pop_back(), Some(7));
+    assert_eq!(list.pop_front(), Some(8));
+    assert_eq!(list.pop_back(), Some(5));
+    assert_eq!(list.pop_front(), Some(6));
+
+    assert_eq!(list.pop_front(), None);
+    assert_eq!(list.pop_back(), None);
   }
 
   #[test]

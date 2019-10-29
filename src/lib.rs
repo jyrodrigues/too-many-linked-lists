287d6a121fe1a9dd6cd5d3a1d01522d72441b262
José Rodrigues <jyrodrigues@gmail.com>
2019-10-28 23:58:54 -0300

    Rename files for posterity

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-pub mod first;
-pub mod second;
-pub mod third;
-pub mod fourth;
-pub mod unsafe_one;
-pub mod singly_linked;
+pub mod ch2_warmup;
+pub mod ch3_singly_linked;
+pub mod ch4_immutable;
+pub mod ch5_mutable_deque_without_refs;
+pub mod ch6_unsafe_singly_linked;
+pub mod singly_linked_by_myself;
 pub mod unsafe_deque;
